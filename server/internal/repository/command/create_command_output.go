d59fed667ab9638ff5d563620d6b38dbe6c7aca7
lumorow <lukazhovtovski@gmail.com>
2024-03-11 01:21:26 +0700

    write output scripts to DB: DONE

--- server/internal/repository/command/create_command_output.go
+++ server/internal/repository/command/create_command_output.go
@@ -1,10 +1,13 @@
 package command
 
-import "context"
+import (
+	"context"
+)
 
 func (r *Repository) CreateCommandOutput(ctx context.Context, id int64, output string) error {
-	query := "INSERT INTO commands_output(id_command, output, time) VALUES ($1,$2, NOW())"
-	err := r.db.QueryRowContext(ctx, query, id, output).Scan()
+	var lastInsertId int64
+	query := "INSERT INTO commands_output(id_command, output, time) VALUES ($1,$2, NOW()) RETURNING id"
+	err := r.db.QueryRowContext(ctx, query, id, output).Scan(&lastInsertId)
 	if err != nil {
 		return err
 	}
--- server/internal/service/command/runner.go
+++ server/internal/service/command/runner.go
@@ -1,27 +1,36 @@
 package command
 
 import (
-	"bytes"
+	"bufio"
 	"context"
-	"io"
+	"fmt"
 	"log"
 	"os/exec"
 	"sync"
 )
 
+const (
+	ON  = true
+	OFF = false
+)
+
+// Runner check a new scripts for start
 func (s *Service) Runner() {
 	ch := make(chan struct{}, 10)
 	wg := sync.WaitGroup{}
-	ctx := context.Context(context.Background())
+	ctx := context.Background()
 	for {
-		scriptIds, err := s.ScriptsCache.GetAllKeys()
-		if err != nil {
-			log.Println(err)
-		}
+		scriptIds, _ := s.ScriptsCache.GetAllKeys()
 		wg.Add(len(scriptIds))
+		ConsoleMode := OFF
+		if len(scriptIds) == 1 {
+			ConsoleMode = ON
+		}
+
 		for _, id := range scriptIds {
 			ch <- struct{}{}
 			go func(id int64) {
+				fmt.Println(len(scriptIds))
 				defer func() {
 					_ = s.ScriptsCache.Delete(id)
 					_ = s.ExecCmdCache.Delete(id)
@@ -32,32 +41,65 @@ func (s *Service) Runner() {
 				val, _ := s.ScriptsCache.Get(id)
 
 				script := val.(string)
+
+				_ = script
+
 				cmd := exec.Command("/bin/sh", "-c", script)
 				_ = s.ExecCmdCache.Set(id, cmd)
 
-				var outb bytes.Buffer
-				cmd.Stdout = &outb
+				stdout, err := cmd.StdoutPipe()
 
-				if err = cmd.Run(); err != nil {
-					log.Println(err)
+				if err != nil {
+					log.Println("error creating stdout pipe:", err)
 					return
 				}
 
-				for {
-					line, err := outb.ReadString('\n')
-					if err == io.EOF {
-						log.Println(string(line))
-						break
-					}
-					err = s.Repository.CreateCommandOutput(ctx, id, string(line))
-					if err != nil {
-						break
-					}
+				if err := cmd.Start(); err != nil {
+					log.Println("error starting command:", err)
+					return
 				}
 
-				log.Println("command executed successfully!")
+				scanner := bufio.NewScanner(stdout)
+
+				outputScriptCh := make(chan string)
+
+				fmt.Println(ConsoleMode)
+
+				go s.ReadCommandOutput(scanner, outputScriptCh)
+
+				s.WriteCommandOutput(ctx, id, outputScriptCh, ConsoleMode)
+
+				if err := scanner.Err(); err != nil {
+					log.Println("error scanning command output:", err)
+				}
+
+				if err := cmd.Wait(); err != nil {
+					log.Println("error waiting for command:", err)
+				} else {
+					log.Println("command executed successfully!")
+				}
 			}(id)
 		}
 		wg.Wait()
+	}
+}
+
+// ReadCommandOutput Read output and write to chan
+func (s *Service) ReadCommandOutput(scanner *bufio.Scanner, outputScriptCh chan string) {
+	defer close(outputScriptCh)
+	for scanner.Scan() {
+		outputScriptCh <- scanner.Text()
+	}
+}
+
+// WriteCommandOutput Write output to DB && console from channel
+func (s *Service) WriteCommandOutput(ctx context.Context, id int64, outputCh chan string, consoleMode bool) {
+	for consoleScriptLine := range outputCh {
+		if consoleMode {
+			log.Println(consoleScriptLine)
+		}
+		if err := s.Repository.CreateCommandOutput(ctx, id, consoleScriptLine); err != nil {
+			log.Println("Error writing command output to database:", err)
+		}
 	}
 }
