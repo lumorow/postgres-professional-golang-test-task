cb07b3a7a57ed737b8bce1673f0db767b3143133
lumorow <lukazhovtovski@gmail.com>
2024-03-13 00:22:25 +0700

    add tests and schema project

--- server/internal/handler/command/command_handler_test.go
+++ server/internal/handler/command/command_handler_test.go
@@ -16,14 +16,12 @@ import (
 // mock gin context
 func GetTestGinContext(w *httptest.ResponseRecorder) *gin.Context {
 	gin.SetMode(gin.TestMode)
-
 	ctx, _ := gin.CreateTestContext(w)
 
 	return ctx
 }
 
 func TestHandler_CreateCommand(t *testing.T) {
-	w := httptest.NewRecorder()
 	type want struct {
 		res    string
 		status int
@@ -31,31 +29,25 @@ func TestHandler_CreateCommand(t *testing.T) {
 	type fields struct {
 		Service *mock.MockService
 	}
-	type args struct {
-		c *gin.Context
-	}
+
 	tests := []struct {
 		name    string
 		fields  fields
 		wantErr bool
 		want    want
-		args    args
-		prepare func(args args, fields fields)
+		prepare func(fields fields, recorder *httptest.ResponseRecorder, c *gin.Context)
 	}{
 		{
-			name:    "test_1, error not expected",
+			name:    "test_1, StatusOK",
 			wantErr: false,
 			want: want{
 				res:    "{\"id\":\"1\",\"script\":\"ls\",\"description\":\"look files\"}",
 				status: http.StatusOK,
 			},
-			args: args{
-				c: GetTestGinContext(w),
-			},
-			prepare: func(args args, fields fields) {
-				args.c.Request = httptest.NewRequest(http.MethodPost, "/command", strings.NewReader(`{"script": "ls", "description": "look files"}`))
-				args.c.Request.Header.Set("Content-Type", "application/json")
-				fields.Service.EXPECT().CreateCommand(args.c.Request.Context(), &entity.CreateCommandReq{
+			prepare: func(fields fields, recorder *httptest.ResponseRecorder, c *gin.Context) {
+				c.Request = httptest.NewRequest(http.MethodPost, "/command", strings.NewReader(`{"script": "ls", "description": "look files"}`))
+				c.Request.Header.Set("Content-Type", "application/json")
+				fields.Service.EXPECT().CreateCommand(c.Request.Context(), &entity.CreateCommandReq{
 					Script:      "ls",
 					Description: "look files",
 				}).Return(&entity.CreateCommandRes{
@@ -63,6 +55,17 @@ func TestHandler_CreateCommand(t *testing.T) {
 					Script:      "ls",
 					Description: "look files",
 				}, nil)
+			},
+		}, {
+			name:    "test_2, StatusBadRequest",
+			wantErr: false,
+			want: want{
+				res:    "{\"error\":\"script must not be empty\"}",
+				status: http.StatusBadRequest,
+			},
+			prepare: func(fields fields, recorder *httptest.ResponseRecorder, c *gin.Context) {
+				c.Request = httptest.NewRequest(http.MethodPost, "/command", strings.NewReader(`{"script": "", "description": "look files"}`))
+				c.Request.Header.Set("Content-Type", "application/json")
 			},
 		},
 	}
@@ -73,8 +76,12 @@ func TestHandler_CreateCommand(t *testing.T) {
 				Service: mock.NewMockService(ctrl),
 			}
 			handler := NewHandler(f.Service)
-			tt.prepare(tt.args, f)
-			handler.CreateCommand(tt.args.c)
+
+			w := httptest.NewRecorder()
+			c := GetTestGinContext(w)
+			tt.prepare(f, w, c)
+
+			handler.CreateCommand(c)
 
 			resp := w.Result()
 
@@ -91,25 +98,63 @@ func TestHandler_CreateCommand(t *testing.T) {
 }
 
 func TestHandler_DeleteCommandById(t *testing.T) {
+	type want struct {
+		res    string
+		status int
+	}
 	type fields struct {
-		Service Service
+		Service *mock.MockService
 	}
-	type args struct {
-		c *gin.Context
-	}
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name    string
+		fields  fields
+		wantErr bool
+		want    want
+		prepare func(fields fields, recorder *httptest.ResponseRecorder, c *gin.Context)
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "test_1, StatusOK",
+			wantErr: false,
+			want: want{
+				res:    "{\"message\":\"command deleted\"}",
+				status: http.StatusOK,
+			},
+			prepare: func(fields fields, recorder *httptest.ResponseRecorder, c *gin.Context) {
+				route := "/command/{id}"
+				c.Request = httptest.NewRequest(http.MethodDelete, route, nil)
+				c.Params = []gin.Param{{
+					Key:   "id",
+					Value: "1",
+				}}
+				c.Request.Header.Set("Content-Type", "application/json")
+				fields.Service.EXPECT().DeleteCommandById(c.Request.Context(), "1").Return(nil)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := &Handler{
-				Service: tt.fields.Service,
+			ctrl := gomock.NewController(t)
+			f := fields{
+				Service: mock.NewMockService(ctrl),
 			}
-			h.DeleteCommandById(tt.args.c)
+			handler := NewHandler(f.Service)
+
+			w := httptest.NewRecorder()
+			c := GetTestGinContext(w)
+			tt.prepare(f, w, c)
+			handler.DeleteCommandById(c)
+
+			resp := w.Result()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.Equal(t, tt.want.status, resp.StatusCode)
+			require.Equal(t, tt.want.res, string(body))
+
 		})
 	}
 }
